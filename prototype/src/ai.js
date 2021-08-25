6cb406ee912beb35f2c5041f65677db07db803b3
jens <jens.schulze@tu-dortmund.de>
2021-08-25 19:56:09 +0200

    ai uses legalMoves instead of emptyPoints

--- prototype/src/ai.js
+++ prototype/src/ai.js
@@ -15,15 +15,7 @@ module.exports = {
       // do smart logic
     //console.log(legalMoves.length);
       // return number in [0-80] for your move
-    const emptyPoints = [];
-    let emptyPointsCounter = 0
-    for (var i=0; i<81; i++){
-      if (currentGame[i] === 0){
-        emptyPoints[emptyPointsCounter] = i;
-        emptyPointsCounter++;
-      }
-    }
-    const baseline = emptyPoints[Math.floor(Math.random()*emptyPoints.length)]
+    const baseline = legalMoves[Math.floor(Math.random() * legalMoves.length)]
     return baseline;
   }
 
