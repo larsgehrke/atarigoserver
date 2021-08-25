913c4bb64958ecfb62e1486e1f9b430123f25a3c
Lars A. Gehrke <accounts@larsgehrke.de>
2021-08-25 11:16:06 +0200

    restructuring for team work

---
+++ prototype/src/ai.js
@@ -0,0 +1,14 @@
+
+/*
+    currentGame: Array with length 80
+    -> representing 9x9 field
+    positive Numbers: black Stone
+    negative Numbers: white Stone
+    0: empty
+*/
+function playAIMove(currentGame) {
+    // do smart logic
+
+    // return number in [0-80] for your move
+  return 0;
+}
