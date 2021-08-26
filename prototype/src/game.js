8bced319efc9d77269d5392e2942f3eff080dc71
Lars A. Gehrke <accounts@larsgehrke.de>
2021-08-26 15:16:28 +0200

    let B play further after win and reload

--- prototype/src/game.js
+++ prototype/src/game.js
@@ -251,7 +251,9 @@ class Game extends React.Component {
     }
     
     if(isB) {
+      if(this.winner === null) {
       this.bIsNext = !this.bIsNext;
+      }
       this.setState({
         history: history.concat([{
           squares: field,
@@ -259,7 +261,9 @@ class Game extends React.Component {
         stepNumber: history.length,
       }, aiTurn);
     } else {
+      if(this.winner === null) {
       this.bIsNext = !this.bIsNext;
+      }
       this.setState({
         history: history.concat([{
           squares: field,
